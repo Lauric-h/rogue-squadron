84ab741b29b999f7ec58bbd0d7839cc6e7255b1d
Lauric-h <h.lauric@outlook.com>
2021-10-22 11:25:42 +0200

    first  commit

---
+++ src/main/java/com/springmsa/roguesquadron/RoguesquadronApplication.java
@@ -0,0 +1,13 @@
+package com.springmsa.roguesquadron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoguesquadronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoguesquadronApplication.class, args);
+	}
+
+}
